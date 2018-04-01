13c8227dd65f4985da3c1ca28f813c571d67e812
Glitch (cypress-seaplane) <none>
2018-04-01 12:39:42 +0000

    🌆👨 Updated with Glitch

--- WolfGang/_MiniMap.js
+++ WolfGang/_MiniMap.js
@@ -2,12 +2,22 @@
 /* globals Script */
 
 (function() {
+    
+  var svg = Script.require("../lib/svg.js?" + Date.now());
   
-  var svg = Script.require("../lib/svg.js");
+  var SVG = svg.SVG;
+  var SVGGroup = svg.SVGGroup;
+  var SVGRect = svg.SVGRect;
+  
+  var s = new SVG();
+  var g = new SVGGroup();
+  var rect = new SVGRect({width:10,height:10},{x:20,y:20});
+  g.addChild(rect);
+  s.addChild(g);
+  
+  console.log(s.serialize());
   
   Script.scriptEnding.connect(scriptEnding);
-  
-  console.log(svg);
   
   function scriptEnding(){
     
